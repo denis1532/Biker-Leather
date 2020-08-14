c09806851e86bac82bd104dcc237419793a06be3
denis.slavenko <denis-slavenko1999@mail.ru>
2020-08-14 18:16:23 +0300

    Added project's folder

---
+++ final-project/js/slider.js
@@ -0,0 +1,10 @@
+$('.sl').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: '2000',
+  cssEase: 'ease-out',
+  fade: true,
+  draggable: false,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+});
